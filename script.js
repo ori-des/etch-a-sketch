e6fdeedf59c9bf4ccde687fc0207dfa8155412af
ori-des <151224246+ori-des@users.noreply.github.com>
2024-01-23 17:15:25 +0100

    Remove old commented code

--- script.js
+++ script.js
@@ -5,11 +5,3 @@ for (let i = 0; i < 256; i++) {
     div.classList.add('grid');
     container.appendChild(div);
 }
-
-// const grid = document.querySelector('.grid');
-
-// for (let i = 0; i < 15; i++) {
-//   let div = document.createElement('div');
-//   grid.appendChild(div);
-//   div.textContent = "Test";
-// }
