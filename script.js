f0332bdf4e5446b39738df5eb4159083f45c8243
ori-des <151224246+ori-des@users.noreply.github.com>
2024-01-23 16:24:26 +0100

    Add class to divs

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const container = document.querySelector('#container');
 
 for (i = 0; i < 16; i++) {
     let div = document.createElement('div');
+    div.classList.add('grid');
     container.appendChild(div);
     div.textContent = "Test";
 }
