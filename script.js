f935510db5ad260a2e4a503aec401de8442206c5
ori-des <151224246+ori-des@users.noreply.github.com>
2024-01-24 17:58:41 +0100

    Add height to container and envelope loop in function

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const container = document.querySelector('#container');
 
+ function createSmallGrid() {
 for (let i = 0; i < 256; i++) {
     let div = document.createElement('div');
     div.classList.add('grid');
     container.appendChild(div);
-}
+    }
+};
 
 function addClass(event) {
     event.target.classList.add('sketch');
